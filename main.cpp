6d5029f58f7d683089765e11744d8e2829c1758d
rishikar <rishikar9849@gmail.com>
2023-06-18 23:57:49 +0530

    Day 1

--- main.cpp
+++ main.cpp
@@ -343,5 +343,39 @@ long long 8 bytes
 
 // }
 
+//operations on Data
+// + - * / %
+// division only returns int not float/decimal  modulus returns reminder 
+
+
+//precedence(which operation first) and assosiativity(which direction left to right or vice versa)
+// multiple operators in same expression one line
+
+// int main(){
+
+//     int a {6};
+//     int b {3};
+//     int c {8};
+//     int d {9};
+//     int e {3};
+//     int f {2};
+//     int g {5};
+        
+
+//         // M D A S A
+//     int result = a + b * c -d/e -f + g; //  6 +  24  -   3 - 2 + 5
+    
+//     std::cout << "result : " << result << std::endl;
+
+//     result = a/b*c +d - e + f;  //   16 + 9 - 3 + 2
+//     std::cout << "result : " << result << std::endl;
+
+//     result = (a + b) * c -d/e -f + g; // 72-3-2+5
+//     std::cout << "result () : " << result << std::endl;
+
+//     // use () for precedence
+   
+//     return 0;
+// }
 
 
