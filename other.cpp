368213d283573960c13d6c998ef28b968c3acf9f
rishikar <rishikar9849@gmail.com>
2023-06-19 17:35:32 +0530

    day 2

---
+++ other.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <iomanip>
+#include <ios>
+
+int main(){
+
+// std::cout << std::left;
+// std::cout << std::setw(10) << "Name" << std::setw(5)<< "Age" << std::endl; 
+// std::cout << std::setw(10) << "rishikar" << std::setw(5)<< "12" << std::endl; 
+// std::cout << std::setw(10) << "rishi" << std::setw(5)<< "12" << std::endl; 
+
+// std::cout << std:: internal;
+// std:: cout << std::setw(20) << -123 ;
+
+
+// int col_width = 20;
+
+
+// std::cout << std::left;
+// std::cout<< std::setfill('-');
+// std:: cout << std::setw(col_width) << "rishikar" << std::setw(col_width)<< "rishi" << std::setw(col_width) << "bhavan" << std::endl;
+// return 0;
+
+
+// int pos = +34;
+
+// std::cout << std::noshowpos;
+
+// std::cout << pos << std::endl;
+
+
+// int pos_int = 717171;
+
+// std:: cout << std::dec << pos_int << std::endl;
+// std:: cout << std::oct << pos_int << std::endl;
+// std:: cout << std::hex << pos_int << std::endl;
+
+
+// auto name = "rishi";
+
+// std::cout << std::uppercase << name << std::endl;
+
+
+// double integer = 3.0;
+// std:: cout << std:: showpoint << integer << std::endl; // or
+// std::cout  << integer <<std:: endl;
+// std:: cout << std:: showpoint;
+// std::cout  << integer <<std:: endl;
+
+
+
+
+
+
+}
